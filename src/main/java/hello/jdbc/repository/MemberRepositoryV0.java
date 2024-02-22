b5f90b5408820d5bb7a82edde2ea0b80a07b0ccb
caaano <wlsrl0515@naver.com>
2024-02-22 16:33:59 +0900

    JDBC - 조회

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -3,6 +3,8 @@ import hello.jdbc.connection.DBConnectionUtil;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import java.sql.*;
+import java.util.NoSuchElementException;
+
 /**
  * JDBC - DriverManager 사용
  */
@@ -26,6 +28,37 @@ public class MemberRepositoryV0 {
             close(con, pstmt, null);
         }
     }
+
+    public Member findbyId(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }finally {
+            close(con, pstmt, rs);
+        }
+    }
+
     private void close(Connection con, Statement stmt, ResultSet rs) {
         if (rs != null) {
             try {
