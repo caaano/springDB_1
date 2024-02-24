f640e3b882446ac3c58b41043a75814dd57e36b5
caaano <wlsrl0515@naver.com>
2024-02-24 16:29:35 +0900

    런타임 예외 적용

--- src/main/java/hello/jdbc/repository/MemberRepositoryEx.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryEx.java
@@ -9,5 +9,4 @@ public interface MemberRepositoryEx {
     Member findById(String memberId) throws SQLException;
     void update(String memberId, int money) throws SQLException;
     void delete(String memberId) throws SQLException;
-
 }
